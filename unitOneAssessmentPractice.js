c21dfcd4b4bb0c45e6a2ffed2f6218733ec61136
tawnyblvd <margara.sees@gmail.com>
2019-09-09 20:34:57 -0400

    updating for current restart

--- unitOneAssessmentPractice.js
+++ unitOneAssessmentPractice.js
@@ -7,8 +7,8 @@ function double(num) {
   return num*2;
 }
 double(14);
-// Uncomment out the next line to test your solution
-runQ1Tests()
+// // Uncomment out the next line to test your solution
+// runQ1Tests()
 
 // Question Two:
 
@@ -127,303 +127,303 @@ runQ1Tests()
 // runQ15Tests()
 
 // The code below is used to test your solutions.  Feel free to look over it, but do not change any of it.
-
-function TestCase(input, output) {
-  this.input = input
-  this.output = output
-  this.formattedInput = () => {
-    return JSON.stringify(this.input)
-  }
-}
-
-function runTests(questionNum, testCases, testCallback) {
-  console.log(`Question ${questionNum} Tests`)
-  try {
-    for (let testCase of testCases) {
-      console.log(`Running ${testCallback.name}(${testCase.formattedInput()})`)
-      assert.strictEqual(JSON.stringify(testCallback(testCase.input)), JSON.stringify(testCase.output))
-    }
-    console.log(`All Question ${questionNum} tests passed!\n`)
-  }
-  catch(error) {
-    if (error.expected === undefined) {
-      console.log("An unexpected error occurred running the test")
-      console.log(error)
-    } else {
-      console.log(`\nTest failed.  Was expecting "${error.expected}", but got "${error.actual}"`)
-    }
-  }
-}
-
-function runQ1Tests() {
-  let testCases = [
-    new TestCase(3,6),
-    new TestCase(0,0),
-    new TestCase(1,2),
-    new TestCase(-8,-16),
-    new TestCase(100.1,200.2)
-  ]
-  runTests("One", testCases, double)
-}
-
-function runQ2Tests() {
-  let testCases = [
-    new TestCase(7, true),
-    new TestCase(84393, false),
-    new TestCase(0, false),
-    new TestCase(0.4927493, true)
-  ]
-  runTests("Two", testCases, containsSeven)
-}
-
-function runQ3Tests() {
-  let testCases = [
-    new TestCase("Hello world!", "Hello world!"),
-    new TestCase("apple", "Apple"),
-    new TestCase("aAaaAaaA", "AAAAAAAA"),
-    new TestCase("", ""),
-    new TestCase("cApitalIZe ThE 'a'S", "cApitAlIZe ThE 'A'S"),
-  ]
-  runTests("Three", testCases, capitalizeTheAs)
-}
-
-function runQ4Tests() {
-  let testCases = [
-    new TestCase([1,2,3,4,5,6,7,8], 8),
-    new TestCase([8,7,6,5,4,3,2,1], 8),
-    new TestCase([3,6,4,8,4,20,6,8,9], 20),
-    new TestCase([-8,-9,-10,-5,-7], -5),
-    new TestCase([0], 0)
-  ]
-  runTests("Four", testCases, largest)
-}
-
-function runQ5Tests() {
-  let testCases = [
-    new TestCase([3,4,5], 4),
-    new TestCase([2,5,7,9], 5.75),
-    new TestCase([3], 3),
-    new TestCase([2,5,7,9], 5.75),
-    new TestCase([-3,4,-7], -2),
-    new TestCase([-2,-9,-4,15], 0),
-  ]
-  runTests("Five", testCases, average)
-}
-
-function runQ6Tests() {
-  let testCases = [
-    new TestCase([1,2,3,4,5,2],2),
-    new TestCase([1,1,1,1,1,1,2],1),
-    new TestCase([1,1,1,1,1,1,2,2,2,2,2,2,2,2,2],2),
-    new TestCase([1],1),
-    new TestCase([1,1,1,2,2,2,1,1],1)
-  ]
-  runTests("Six", testCases, mode)
-}
-
-function runQ7Tests() {
-  let testCases = [
-    new TestCase([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31], 13),
-    new TestCase([12, 3, 5], 5),
-    new TestCase([3, 13, 7, 5, 21, 23, 39, 23, 40, 23, 14, 12, 56, 23, 29], 23),
-    new TestCase([3, 13, 7, 5, 21, 23, 23, 40, 23, 14, 12, 56, 23, 29], 22),
-    new TestCase([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], 30.5)
-  ]
-  runTests("Seven", testCases, median)
-}
-
-function runQ8Tests() {
-  let testCases = [
-    new TestCase(
-      [
-        {teacher: "Mr. Smith", numberOfStudents: 28},
-        {teacher: "Ms. Lopez", numberOfStudents: 32},
-        {teacher: "Professor McGonagall", numberOfStudents: 20}
-      ], 80
-    ),
-    new TestCase(
-      [
-        {teacher: "Ms. A", numberOfStudents: 30},
-        {teacher: "Ms. B", numberOfStudents: 20},
-        {teacher: "Mr. C", numberOfStudents: 25},
-      ], 75
-    ),
-    new TestCase(
-      [
-        {teacher: "Ms. D", numberOfStudents: 33},
-        {teacher: "Mr. E", numberOfStudents: 10},
-      ], 43
-    ),
-    new TestCase(
-      [
-        {teacher: "Mr. F", numberOfStudents: 1},
-      ], 1
-    )
-  ]
-  runTests("Eight", testCases, addAllStudents)
-}
-
-function runQ9Tests() {
-  let testCases = [
-    new TestCase(
-      [
-        {teacher: "Mr. Smith", numberOfStudents: 28},
-        {teacher: "Ms. Lopez", numberOfStudents: 32},
-        {teacher: "Professor McGonagall", numberOfStudents: 20}
-      ], {teacher: "Professor McGonagall", numberOfStudents: 20}
-    ),
-    new TestCase(
-      [
-        {teacher: "Ms. A", numberOfStudents: 30},
-        {teacher: "Ms. B", numberOfStudents: 20},
-        {teacher: "Mr. C", numberOfStudents: 25},
-      ], {teacher: "Ms. B", numberOfStudents: 20}
-    ),
-    new TestCase(
-      [
-        {teacher: "Ms. D", numberOfStudents: 33},
-        {teacher: "Mr. E", numberOfStudents: 10},
-      ], {teacher: "Mr. E", numberOfStudents: 10}
-    ),
-    new TestCase(
-      [
-        {teacher: "Mr. F", numberOfStudents: 1},
-      ], {teacher: "Mr. F", numberOfStudents: 1}
-    )
-  ]
-  runTests("Nine", testCases, fewestStudents)
-}
-
-function runQ10Tests() {
-  let testCases = [
-    new TestCase(
-      [1,2,3,4,5,6,7],
-      [2,4,6,8,10,12,14]
-    ),
-    new TestCase(
-      [-2,-5,3],
-      [-4,-10,6]
-    ),
-    new TestCase(
-      [1.3,9.1,2.4],
-      [2.6,18.2,4.8]
-    ),
-    new TestCase(
-      [],
-      []
-    ),
-    new TestCase(
-      [0],
-      [0]
-    )
-  ]
-  runTests("Ten", testCases, doubleAllElements)
-}
-
-function runQ11Tests() {
-  let testCases = [
-    new TestCase(
-      ["aaaa", "aa", "a", "aaaaaa"],
-      ["aaaa", "aaaaaa"]
-    ),
-    new TestCase(
-      ["abcd", "       ", ""],
-      ["abcd", "       "]
-    ),
-    new TestCase(
-      [".......", ".", "'''''''"],
-      [".......", "'''''''"]
-    )
-  ]
-  runTests("Eleven", testCases, onlyLongStrings)
-}
-
-function runQ12Tests() {
-  let testCases = [
-    new TestCase(
-      [4, "aaaa", 43, 5, "aa", "a", "aaaaaa"],
-      false
-    ),
-    new TestCase(
-      [3,5,2,56,7,3.9],
-      true
-    ),
-    new TestCase(
-      ["4","453","456789", "484.2"],
-      true
-    ),
-    new TestCase(
-      ["one", "two", "three"],
-      false
-    ),
-    new TestCase(
-      [Infinity, -Infinity, 1000000000, 1e19, 0.000000001],
-      true
-    )
-  ]
-  runTests("Twelve", testCases, containsOnlyNumbers)
-}
-
-function runQ13Tests() {
-  let testCases = [
-    new TestCase(new Person(19, "a"), true),
-    new TestCase(new Person(3, "b"), false),
-    new TestCase(new Person(18, "a"), true),
-    new TestCase(new Person(500, "a"), true),
-    new TestCase(new Person(17, "a"), false),
-  ]
-  runTests("Thirteen", testCases, person => person.isALegalAdult())
-}
-
-function runQ14Tests() {
-  let testCases = [
-    new TestCase( [
-      new Person(19, "a"),
-      new Person(16, "b"),
-      new Person(24, "c"),
-      new Person(18, "d"),
-    ],
-    [
-      new Person(19, "a"),
-      new Person(24, "c"),
-      new Person(18, "d"),
-    ]),
-  new TestCase( [
-    new Person(11, "a"),
-    new Person(16, "b"),
-    new Person(11, "c"),
-    new Person(8, "d"),
-    ],
-    []),
-    new TestCase( [
-      new Person(8, "a"),
-      new Person(16, "b"),
-      new Person(64, "c"),
-      new Person(18, "d"),
-    ],
-    [
-      new Person(64, "c"),
-      new Person(18, "d"),
-    ])
-  ]
-  runTests("Fourteen", testCases, getAllAdults)
-}
-
-function runQ15Tests() {
-  let testCases = [
-    new TestCase( [
-      new Person(19, "Adam"),
-      new Person(16, "Beth"),
-      new Person(24, "Cam"),
-      new Person(18, "Dan"),
-    ], "Adam,Beth,Cam,Dan"),
-    new TestCase( [
-      new Person(99, "Eve"),
-      new Person(1, "Fran"),
-    ], "Eve,Fran"),
-    new TestCase( [
-      new Person(66, "Gene"),
-    ], "Gene"),
-  ]
-  runTests("Fifteen", testCases, getAllNames)
-}
+//
+// function TestCase(input, output) {
+//   this.input = input
+//   this.output = output
+//   this.formattedInput = () => {
+//     return JSON.stringify(this.input)
+//   }
+// }
+//
+// function runTests(questionNum, testCases, testCallback) {
+//   console.log(`Question ${questionNum} Tests`)
+//   try {
+//     for (let testCase of testCases) {
+//       console.log(`Running ${testCallback.name}(${testCase.formattedInput()})`)
+//       assert.strictEqual(JSON.stringify(testCallback(testCase.input)), JSON.stringify(testCase.output))
+//     }
+//     console.log(`All Question ${questionNum} tests passed!\n`)
+//   }
+//   catch(error) {
+//     if (error.expected === undefined) {
+//       console.log("An unexpected error occurred running the test")
+//       console.log(error)
+//     } else {
+//       console.log(`\nTest failed.  Was expecting "${error.expected}", but got "${error.actual}"`)
+//     }
+//   }
+// }
+//
+// function runQ1Tests() {
+//   let testCases = [
+//     new TestCase(3,6),
+//     new TestCase(0,0),
+//     new TestCase(1,2),
+//     new TestCase(-8,-16),
+//     new TestCase(100.1,200.2)
+//   ]
+//   runTests("One", testCases, double)
+// }
+//
+// function runQ2Tests() {
+//   let testCases = [
+//     new TestCase(7, true),
+//     new TestCase(84393, false),
+//     new TestCase(0, false),
+//     new TestCase(0.4927493, true)
+//   ]
+//   runTests("Two", testCases, containsSeven)
+// }
+//
+// function runQ3Tests() {
+//   let testCases = [
+//     new TestCase("Hello world!", "Hello world!"),
+//     new TestCase("apple", "Apple"),
+//     new TestCase("aAaaAaaA", "AAAAAAAA"),
+//     new TestCase("", ""),
+//     new TestCase("cApitalIZe ThE 'a'S", "cApitAlIZe ThE 'A'S"),
+//   ]
+//   runTests("Three", testCases, capitalizeTheAs)
+// }
+//
+// function runQ4Tests() {
+//   let testCases = [
+//     new TestCase([1,2,3,4,5,6,7,8], 8),
+//     new TestCase([8,7,6,5,4,3,2,1], 8),
+//     new TestCase([3,6,4,8,4,20,6,8,9], 20),
+//     new TestCase([-8,-9,-10,-5,-7], -5),
+//     new TestCase([0], 0)
+//   ]
+//   runTests("Four", testCases, largest)
+// }
+//
+// function runQ5Tests() {
+//   let testCases = [
+//     new TestCase([3,4,5], 4),
+//     new TestCase([2,5,7,9], 5.75),
+//     new TestCase([3], 3),
+//     new TestCase([2,5,7,9], 5.75),
+//     new TestCase([-3,4,-7], -2),
+//     new TestCase([-2,-9,-4,15], 0),
+//   ]
+//   runTests("Five", testCases, average)
+// }
+//
+// function runQ6Tests() {
+//   let testCases = [
+//     new TestCase([1,2,3,4,5,2],2),
+//     new TestCase([1,1,1,1,1,1,2],1),
+//     new TestCase([1,1,1,1,1,1,2,2,2,2,2,2,2,2,2],2),
+//     new TestCase([1],1),
+//     new TestCase([1,1,1,2,2,2,1,1],1)
+//   ]
+//   runTests("Six", testCases, mode)
+// }
+//
+// function runQ7Tests() {
+//   let testCases = [
+//     new TestCase([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31], 13),
+//     new TestCase([12, 3, 5], 5),
+//     new TestCase([3, 13, 7, 5, 21, 23, 39, 23, 40, 23, 14, 12, 56, 23, 29], 23),
+//     new TestCase([3, 13, 7, 5, 21, 23, 23, 40, 23, 14, 12, 56, 23, 29], 22),
+//     new TestCase([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], 30.5)
+//   ]
+//   runTests("Seven", testCases, median)
+// }
+//
+// function runQ8Tests() {
+//   let testCases = [
+//     new TestCase(
+//       [
+//         {teacher: "Mr. Smith", numberOfStudents: 28},
+//         {teacher: "Ms. Lopez", numberOfStudents: 32},
+//         {teacher: "Professor McGonagall", numberOfStudents: 20}
+//       ], 80
+//     ),
+//     new TestCase(
+//       [
+//         {teacher: "Ms. A", numberOfStudents: 30},
+//         {teacher: "Ms. B", numberOfStudents: 20},
+//         {teacher: "Mr. C", numberOfStudents: 25},
+//       ], 75
+//     ),
+//     new TestCase(
+//       [
+//         {teacher: "Ms. D", numberOfStudents: 33},
+//         {teacher: "Mr. E", numberOfStudents: 10},
+//       ], 43
+//     ),
+//     new TestCase(
+//       [
+//         {teacher: "Mr. F", numberOfStudents: 1},
+//       ], 1
+//     )
+//   ]
+//   runTests("Eight", testCases, addAllStudents)
+// }
+//
+// function runQ9Tests() {
+//   let testCases = [
+//     new TestCase(
+//       [
+//         {teacher: "Mr. Smith", numberOfStudents: 28},
+//         {teacher: "Ms. Lopez", numberOfStudents: 32},
+//         {teacher: "Professor McGonagall", numberOfStudents: 20}
+//       ], {teacher: "Professor McGonagall", numberOfStudents: 20}
+//     ),
+//     new TestCase(
+//       [
+//         {teacher: "Ms. A", numberOfStudents: 30},
+//         {teacher: "Ms. B", numberOfStudents: 20},
+//         {teacher: "Mr. C", numberOfStudents: 25},
+//       ], {teacher: "Ms. B", numberOfStudents: 20}
+//     ),
+//     new TestCase(
+//       [
+//         {teacher: "Ms. D", numberOfStudents: 33},
+//         {teacher: "Mr. E", numberOfStudents: 10},
+//       ], {teacher: "Mr. E", numberOfStudents: 10}
+//     ),
+//     new TestCase(
+//       [
+//         {teacher: "Mr. F", numberOfStudents: 1},
+//       ], {teacher: "Mr. F", numberOfStudents: 1}
+//     )
+//   ]
+//   runTests("Nine", testCases, fewestStudents)
+// }
+//
+// function runQ10Tests() {
+//   let testCases = [
+//     new TestCase(
+//       [1,2,3,4,5,6,7],
+//       [2,4,6,8,10,12,14]
+//     ),
+//     new TestCase(
+//       [-2,-5,3],
+//       [-4,-10,6]
+//     ),
+//     new TestCase(
+//       [1.3,9.1,2.4],
+//       [2.6,18.2,4.8]
+//     ),
+//     new TestCase(
+//       [],
+//       []
+//     ),
+//     new TestCase(
+//       [0],
+//       [0]
+//     )
+//   ]
+//   runTests("Ten", testCases, doubleAllElements)
+// }
+//
+// function runQ11Tests() {
+//   let testCases = [
+//     new TestCase(
+//       ["aaaa", "aa", "a", "aaaaaa"],
+//       ["aaaa", "aaaaaa"]
+//     ),
+//     new TestCase(
+//       ["abcd", "       ", ""],
+//       ["abcd", "       "]
+//     ),
+//     new TestCase(
+//       [".......", ".", "'''''''"],
+//       [".......", "'''''''"]
+//     )
+//   ]
+//   runTests("Eleven", testCases, onlyLongStrings)
+// }
+//
+// function runQ12Tests() {
+//   let testCases = [
+//     new TestCase(
+//       [4, "aaaa", 43, 5, "aa", "a", "aaaaaa"],
+//       false
+//     ),
+//     new TestCase(
+//       [3,5,2,56,7,3.9],
+//       true
+//     ),
+//     new TestCase(
+//       ["4","453","456789", "484.2"],
+//       true
+//     ),
+//     new TestCase(
+//       ["one", "two", "three"],
+//       false
+//     ),
+//     new TestCase(
+//       [Infinity, -Infinity, 1000000000, 1e19, 0.000000001],
+//       true
+//     )
+//   ]
+//   runTests("Twelve", testCases, containsOnlyNumbers)
+// }
+//
+// function runQ13Tests() {
+//   let testCases = [
+//     new TestCase(new Person(19, "a"), true),
+//     new TestCase(new Person(3, "b"), false),
+//     new TestCase(new Person(18, "a"), true),
+//     new TestCase(new Person(500, "a"), true),
+//     new TestCase(new Person(17, "a"), false),
+//   ]
+//   runTests("Thirteen", testCases, person => person.isALegalAdult())
+// }
+//
+// function runQ14Tests() {
+//   let testCases = [
+//     new TestCase( [
+//       new Person(19, "a"),
+//       new Person(16, "b"),
+//       new Person(24, "c"),
+//       new Person(18, "d"),
+//     ],
+//     [
+//       new Person(19, "a"),
+//       new Person(24, "c"),
+//       new Person(18, "d"),
+//     ]),
+//   new TestCase( [
+//     new Person(11, "a"),
+//     new Person(16, "b"),
+//     new Person(11, "c"),
+//     new Person(8, "d"),
+//     ],
+//     []),
+//     new TestCase( [
+//       new Person(8, "a"),
+//       new Person(16, "b"),
+//       new Person(64, "c"),
+//       new Person(18, "d"),
+//     ],
+//     [
+//       new Person(64, "c"),
+//       new Person(18, "d"),
+//     ])
+//   ]
+//   runTests("Fourteen", testCases, getAllAdults)
+// }
+//
+// function runQ15Tests() {
+//   let testCases = [
+//     new TestCase( [
+//       new Person(19, "Adam"),
+//       new Person(16, "Beth"),
+//       new Person(24, "Cam"),
+//       new Person(18, "Dan"),
+//     ], "Adam,Beth,Cam,Dan"),
+//     new TestCase( [
+//       new Person(99, "Eve"),
+//       new Person(1, "Fran"),
+//     ], "Eve,Fran"),
+//     new TestCase( [
+//       new Person(66, "Gene"),
+//     ], "Gene"),
+//   ]
+//   runTests("Fifteen", testCases, getAllNames)
+// }
